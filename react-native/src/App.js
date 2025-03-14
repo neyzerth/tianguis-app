8faab17ddd401cdd440a756e5088685688b4895e
SKSKronosUTT <0323105967@ut-tijuana.edu.mx>
2025-03-14 02:47:03 -0700

    individual item screen, but button links are missing

--- react-native/src/App.js
+++ react-native/src/App.js
@@ -11,6 +11,10 @@ import SellerNavigator from './navigation/sellerNavigator';
 //pantalla de añadir item
 import AddItem from './screens/seller/AddItem';
 
+//pantalla de Item individual
+import MyItems from './screens/seller/MyItems';
+import Item from './screens/seller/Item';
+
 const Stack = createStackNavigator();
 
 export default function App() {
@@ -26,6 +30,8 @@ export default function App() {
         <Stack.Screen name="Signup" component={Signup} />
         <Stack.Screen name="SellerHome" component={SellerNavigator} />
         <Stack.Screen name="AddItem" component={AddItem} />
+        <Stack.Screen name="MyItems" component={MyItems} />
+        <Stack.Screen name="Item" component={Item} />
       </Stack.Navigator>
     </NavigationContainer>
   );
