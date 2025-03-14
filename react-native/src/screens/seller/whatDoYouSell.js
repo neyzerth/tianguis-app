5cd995304650b9503ce85b1d91c9ec45980feb97
SKSKronosUTT <0323105967@ut-tijuana.edu.mx>
2025-03-13 19:47:05 -0700

    Finished screen, only linking Pressables left

--- react-native/src/screens/seller/whatDoYouSell.js
+++ react-native/src/screens/seller/whatDoYouSell.js
@@ -1,13 +1,76 @@
 import React, {useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { ScrollView } from 'react-native-web';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Searchbar } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon2 from 'react-native-vector-icons/Ionicons';
+import Icon3 from 'react-native-vector-icons/MaterialCommunityIcons';
+import Icon4 from 'react-native-vector-icons/Ionicons';
 
 export default function App() {
   return (
-    <ScrollView>
-        <Text></Text>
-    </ScrollView>
+    <View style={styles.container}>
+      <Text style={styles.title}>What do you sell?</Text>
+      
+      <View style={styles.search}>
+        <Searchbar
+            placeholder="Search"
+          />
+      </View>
+      
+      <Text style={styles.heading}>Categories</Text>
+
+      <Pressable onPress={() => {    }}>
+        <View style={styles.category}>
+            <Icon 
+              name="soccer-ball-o"
+              size={30}
+            />
+            <Text style={styles.categoryText}>Sports</Text>
+        </View>
+      </Pressable>
+
+      <Pressable onPress={() => {    }}>
+        <View style={styles.category}>
+            <Icon2
+              name="shirt-outline"
+              size={28}
+            />
+            <Text style={styles.categoryText}>Clothes</Text>
+        </View>
+      </Pressable>
+
+      <Pressable onPress={() => {    }}>
+        <View style={styles.category}>
+            <Icon3
+              name="toy-brick-outline"
+              size={30}
+            />
+            <Text style={styles.categoryText}>Toys</Text>
+      </View>
+      </Pressable>
+
+      <Pressable onPress={() => {    }}>
+        <View style={styles.category}>
+            <Icon4 
+              name="game-controller-outline"
+              size={30}
+            />
+            <Text style={styles.categoryText}>Videogames</Text>
+        </View>
+      </Pressable>
+
+
+      <Pressable onPress={() => {    }}>
+        <View style={styles.category}>
+            <Icon4 
+              name="ellipsis-horizontal-circle"
+              size={30}
+            />
+            <Text style={styles.categoryText}>Others</Text>
+        </View>
+      </Pressable>
+    </View>
   );
 }
 
@@ -19,10 +82,30 @@ const styles = StyleSheet.create({
     top: 50
   },
   title: {
-    fontSize: 30
+    fontSize: 40,
+    margin: 20
   },
-  subtitle: {
-    color: '#aaa',
-    fontSize: 15
+  search: {
+    width: 400,
+    margin: 20
   },
+  heading: {
+    color: '#111',
+    fontSize: 30,
+    textAlign: 'left',
+    fontWeight: 'bold',
+    width: 400,
+    margin: 10
+  },
+  category: {
+    width: 400,
+    height: 50,
+    margin: 10,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  categoryText: {
+    fontSize: 25,
+    marginLeft: 20
+  }
 });
