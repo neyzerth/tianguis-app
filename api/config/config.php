76d60c47a1ce5a2a8f5495afc109b8a82df7ff7c
neyzer <neyzertoledo@gmail.com>
2025-03-18 18:09:16 -0700

    config file

---
+++ api/config/config.php
@@ -0,0 +1,8 @@
+<?php
+    class Config {
+        public static $mysql_server = "localhost";
+        public static $mysql_user = "root";
+        public static $mysql_password = "";
+        public static $mysql_database = "tianguisapp";
+    }
+?>
