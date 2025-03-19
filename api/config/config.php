8ff37f52ab09b74ed57ac709b4372824cf632286
neyzer <neyzertoledo@gmail.com>
2025-03-19 11:09:18 -0700

    Stop tracking config.php

--- api/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    class Config {
-        public static $mysql_server = "localhost";
-        public static $mysql_user = "root";
-        public static $mysql_password = "";
-        public static $mysql_database = "tianguisapp";
-    }
-?>
