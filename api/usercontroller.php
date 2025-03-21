edb1edff3582f1e4c5c05e6358a86bf1cbc6979b
neyzer <neyzertoledo@gmail.com>
2025-03-21 09:20:59 -0700

    update user controller

--- api/usercontroller.php
+++ api/usercontroller.php
@@ -1,20 +1,28 @@
 <?php
     require_once('models/user.php');
     require_once('models/message.php');
-    //GET
+
     if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-        if ($parameter != '') 
-            echo User::get($parameter); //get one
-        else 
-            echo User::get(); //get all
+        if ($parameter != '')
+            echo User::get($parameter);
+        else
+            echo User::get();
     }
+
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        if(isset($_POST['id']) && isset($_POST['name'])) {
-            $b = new User($_POST['id'], $_POST['name']);
-            echo $b->add();
+        // Validar que existan los campos requeridos
+        $required_fields = ['first_name', 'last_name', 'email', 'password', 'phone'];
+        $missing_fields = array_filter($required_fields, function($field) {
+            return !isset($_POST[$field]) || empty($_POST[$field]);
+        });
+
+        if (!empty($missing_fields)) {
+            echo Message::get_message(999, 'Missing required fields: ' . implode(', ', $missing_fields));
+            exit;
         }
-        else {
-            echo Message::get_message(999, 'Missing post data');
-        }
+
+        // Crear nuevo usuario con los datos del POST
+        $user = new User($_POST);
+        echo $user->add();
     }
 ?>
